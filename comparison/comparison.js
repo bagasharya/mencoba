3cab7d5b1c1cdd16e0e02b8570f3f6a64338a9e5
bagas harya <bagasharya@s.local>
2023-10-18 22:14:44 +0700

    Tambahan Looping & Comparison

---
+++ comparison/comparison.js
@@ -0,0 +1,44 @@
+// comparison Equal SLIDE 1
+const equal = 10 == 10;
+console.log(equal);
+
+// comparison Not Equal 
+const notEqual =  "huruf" != 10;
+console.log(notEqual);
+
+// comparison Strict Equal
+const strictEqual = 10 === 10;
+console.log(strictEqual);
+
+// comparison Strict Not Equal 
+const strictNotEqual = "String" !== 10;
+console.log(strictNotEqual);
+
+// comparison Greater Than
+const greaterThen = 10 > 5;
+console.log(greaterThen);
+
+// comparison Greater Than or Equal
+const greaterThenOrEqual = 10 >= 10;
+console.log(greaterThenOrEqual);
+
+// comparison Less Than
+const lessThan = 5 < 20;
+console.log(lessThan);
+
+// comparison Less Than or Equal 
+const lessThanOrEqual = 5 <= 5;
+
+
+// logical operator &&
+const and = (100 === 100) && (10 > 1);
+console.log(and);
+
+// logical operator ||
+const or = (11 > 15) || (20 >= 19);
+console.log(or);
+
+// logical operator !
+const not = (! 100 < 10);
+console.log(not);
+
